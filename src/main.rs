5da3a103642bc7af24494f81d9559a68af888165
gorka <gorka.irazoki@gmail.com>
2021-11-15 18:30:41 +0100

    remove rest of the code, importing the file compiles it

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,7 @@ fn load(file_name: &str) -> Result<()> {
     let now = std::time::Instant::now();
     let module = Module::from_file(&engine, &file_name)?;
     println!("imported in {} ms", now.elapsed().as_millis());
-   // A `Store` is what will own instances, functions, globals, etc. All wasm
-    // items are stored within a `Store`, and it's what we'll always be using to
-    // interact with the wasm world. Custom data can be stored in stores but for
-    // now we just use `()`.
-    let mut store = Store::new(&engine, ());
-
-    println!("Instantiate");
-    let instance = Instance::new(&mut store, &module,  &[])?;
-
-    println!("getting the func");
-    // The `Instance` gives us access to various exported functions and items,
-    // which we access here to pull out our `answer` exported function and
-    // run it.
-    let answer = instance.get_func(&mut store, "validate_transaction")
-        .expect("`answer` was not an exported function");
-
-
+  
     Ok(())
 }
 
